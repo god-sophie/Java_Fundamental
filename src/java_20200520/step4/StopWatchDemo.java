db6e961acc9f55caa074cfbcf1fa9131026764d6
god-sophie <jhhan0374@gmail.com>
2020-05-21 09:36:52 +0900

    2020-05-21-v1

--- src/java_20200520/step4/StopWatchDemo.java
+++ src/java_20200520/step4/StopWatchDemo.java
@@ -1,13 +1,14 @@
 package java_20200520.step4;
 
-import java_20200520.step4.StopNanoWatch;
-
 public class StopWatchDemo {
 	
-	public static void run(StopWatch s1) {
+	public static void run(StopMilliWatch s1) {
 		s1.execute();
 	}
 	
+	public static void run(StopNanoWatch s1) {
+		s1.execute();
+	}
 	
 	public static void main(String[] args) {
 		StopNanoWatch s1 = new StopNanoWatch();
